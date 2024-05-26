587c3f160c56fc201c3cb38b8eb608cdd7f71288
Umar Hasnain <umarhasnain045@gmail.com>
2024-05-26 08:29:09 +0800

    Complete with Local Storage

--- app.js
+++ app.js
@@ -1,22 +1,71 @@
 let btn = document.getElementById("btn");
+let todo_input = document.getElementById("todo_input");
+let input_list = document.getElementById("input_list");
+var arr = [];
+
+function checkEnterKey(event) {
+    if (event.key === "Enter") {
+        addTodo();
+    }
+}
+
+var todoList = localStorage.getItem("input_list");
+if (todoList) {
+    arr = JSON.parse(todoList);
+}
+
+function loadAllTodo() {
+    input_list.innerHTML = "";
+    for (var i = 0; i < arr.length; i++) {
+        var li = `
+    <li> ${arr[i]} 
+    <button  class="edit_btn" onclick="deleteOne('${i}')">Del</button>
+    <button  class="edit_btn" onclick="editTodo('${arr[i]}','${i}')">Edit</button> 
+    </li>
+    `;
+        input_list.innerHTML += li;
+    }
+}
+loadAllTodo();
 
 
-btn.addEventListener("click", function() {
-    let todo_input = document.getElementById("todo_input")
-    let input_list = document.getElementById("input_list");
+function addTodo() {
 
+    let todo_input = document.getElementById("todo_input");
+    if (todo_input.value.trim() !== "") {
+        var li = `
+      <li> ${todo_input.value} 
+      <button class="edit_btn" onclick="editTodo('${todo_input.value}','${arr.length}')">Edit</button> 
+      <button class="edit_btn" onclick="deleteOne('${arr.length}')">Del</button>
+      </li>
+      `;
+        arr.push(todo_input.value);
+        localStorage.setItem("input_list", JSON.stringify(arr));
+        input_list.innerHTML += li;
+        todo_input.value = "";
 
+    }
 
-    input_list.innerHTML += `<li>${todo_input.value} </li>`
+};
 
+function deleteAll() {
+    input_list.innerHTML = "";
+    localStorage.removeItem("input_list");
+}
 
+function deleteOne(i) {
+    console.log(i);
+    arr.splice(i, 1);
+    localStorage.setItem("input_list", JSON.stringify(arr));
+    event.target.parentNode.remove();
+    loadAllTodo();
+}
 
-    console.log(todo_input);
-
-    todo_input.value = "";
-
-
-
-
-
-})
+function editTodo(a, i) {
+    var oldValue = prompt("Enter updated value", a);
+    if (oldValue) {
+        arr.splice(i, 1, oldValue);
+        localStorage.setItem("input_list", JSON.stringify(arr));
+        event.target.parentNode.firstChild.nodeValue = oldValue;
+    }
+}
